1a6336135f004446a45897b0f90561bf56aac0f3
Fedor Suhanov <fedorsuhanov@gmail.com>
2025-11-25 22:22:46 +0300

    add ArgHolder

---
+++ include/ArgHolder.h
@@ -0,0 +1,83 @@
+#ifndef ARG_HOLDER_H
+#define ARG_HOLDER_H
+
+#include <memory>
+#include <string>
+#include <exception>
+
+class ArgHolder {
+private:
+    struct Base {
+        virtual Base* get_copy() = 0;
+        virtual ~Base() = default;
+    };
+
+    template<typename T>
+    struct Derived : Base{
+        T value;
+        Derived(const T& value): value(value) {}
+        Derived(T&& value): value(std::move(value)) {}
+        ~Derived() = default;
+
+        Base* get_copy() override {
+            return new Derived(value);
+        }
+
+        T get_value() const {
+            return value;
+        }
+    };
+
+    uint8_t index;
+    std::unique_ptr<Base> value;
+public:
+    ArgHolder() = delete;
+
+    template<typename T>
+    ArgHolder(std::string str, const T& value) 
+        : index(str.back())
+        , value(std::make_unique<Derived<T>>(value)) {}
+    
+    ArgHolder(const ArgHolder& other)
+        : index(other.index)
+        , value(other.value->get_copy()) {}
+    
+    ArgHolder(ArgHolder&& other)
+        : index(other.index)
+        , value(std::move(other.value)) {}
+
+
+    ArgHolder& operator=(const ArgHolder& other) {
+        if(this == &other) {
+            return *this;
+        }
+        value.reset(other.value->get_copy());
+        index = other.index;
+        return *this;
+    }
+
+    ArgHolder& operator=(ArgHolder&& other) {
+        if(this == &other) {
+            return *this;
+        }
+        value = std::move(other.value);
+        index = other.index;
+        return *this;
+    }
+
+    template<typename T>
+    T get_as() const {
+        Derived<T>* arg = dynamic_cast<Derived<T>*>(value.get());
+        if(!arg) {
+            throw std::runtime_error("type of argument is not correct");
+        }
+        return arg->get_value();
+    }
+
+    uint8_t get_index() const {
+        return index;
+    }
+
+};
+
+#endif
